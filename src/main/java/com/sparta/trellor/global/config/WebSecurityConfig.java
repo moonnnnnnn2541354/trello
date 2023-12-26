106b84da400404b692faa8f5c599a71693ff6e99
minahYu <mina010311@naver.com>
2023-12-26 21:41:15 +0900

    feat : 스프링 시큐리티 설정을 담당하는 클래스에 AuthenticationManager를 bean으로 등록하고 반환하는 메서드 추가

--- src/main/java/com/sparta/trellor/global/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/trellor/global/config/WebSecurityConfig.java
@@ -1,15 +1,32 @@
 package com.sparta.trellor.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * AuthenticationManager를 Bean으로 등록하고 반환하는 메서드
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
