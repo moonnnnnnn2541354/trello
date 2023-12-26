f232ea87e3d5dbb918036d8cdfbd0d1a46056509
minahYu <mina010311@naver.com>
2023-12-26 21:10:17 +0900

    feat : 사용자 정보를 제공하는 서비스 구현 클래스 생성

---
+++ src/main/java/com/sparta/trellor/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sparta.trellor.global.security;
+
+import com.sparta.trellor.domain.user.entity.User;
+import com.sparta.trellor.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
