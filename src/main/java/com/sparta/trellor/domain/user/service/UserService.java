17dbf801d3d0e28c4f2ad354f011cf68b1c3127b
minahYu <mina010311@naver.com>
2023-12-26 18:41:12 +0900

    feat : 사용자 서비스 회원가입 관련 메서드 추가

--- src/main/java/com/sparta/trellor/domain/user/service/UserService.java
+++ src/main/java/com/sparta/trellor/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.sparta.trellor.domain.user.service;
 
 import com.sparta.trellor.domain.user.dto.request.SignupRequestDto;
 import com.sparta.trellor.domain.user.entity.User;
+import com.sparta.trellor.domain.user.entity.UserRoleEnum;
 import com.sparta.trellor.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * 회원가입 관련 메서드
+ */
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -22,8 +26,10 @@ public class UserService {
 
         Optional<User> user = userRepository.findByUsernameAndEmail(username, email);
 
-        if(user.isPresent()) {
-
+        if(user.isEmpty()) {
+            userRepository.save(new User(username, email, password, UserRoleEnum.USER));
+        } else {
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
         }
     }
 }
