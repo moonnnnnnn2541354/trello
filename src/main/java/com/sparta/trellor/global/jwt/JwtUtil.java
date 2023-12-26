d75d5df12e3770a447fbebddc224f44cbcb5c6a2
minahYu <mina010311@naver.com>
2023-12-26 20:19:24 +0900

    feat : JwtUtil 클래스에 토큰 검증 메서드 추가

--- src/main/java/com/sparta/trellor/global/jwt/JwtUtil.java
+++ src/main/java/com/sparta/trellor/global/jwt/JwtUtil.java
@@ -1,12 +1,12 @@
 package com.sparta.trellor.global.jwt;
 
 import com.sparta.trellor.domain.user.entity.UserRoleEnum;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -16,6 +16,7 @@ import java.util.Base64;
 import java.util.Date;
 
 @Component
+@Slf4j(topic = "JwtUtil")
 public class JwtUtil {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_KEY = "auth";
@@ -61,5 +62,24 @@ public class JwtUtil {
             return bearerToken.substring(7);
         }
         return null;
+    }
+
+    /**
+     * 토큰 검증하는 메서드
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch(SecurityException | MalformedJwtException | SignatureException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명입니다.");
+        } catch(ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token입니다.");
+        } catch(UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰입니다.");
+        } catch(IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰입니다.");
+        }
+        return false;
     }
 }
