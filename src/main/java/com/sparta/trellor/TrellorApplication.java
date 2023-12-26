68dd051227e8a697a283b1de19617f07f5767f6f
tast <ioae7979@gmail.com>
2023-12-26 12:30:26 +0900

    first commit

---
+++ src/main/java/com/sparta/trellor/TrellorApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.trellor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrellorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/trellor/TrellorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.trellor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
