b4b89dbad25ff29d07a3cc30cc3645abd5db3181
minahYu <mina010311@naver.com>
2023-12-26 16:40:22 +0900

    feat : 사용자 권한을 설정하는 열거형 클래스 생성

--- src/main/java/com/sparta/trellor/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/trellor/domain/user/controller/UserController.java
@@ -1,2 +1,14 @@
-package com.sparta.trellor.domain.user.controller;public class UserController {
+package com.sparta.trellor.domain.user.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @PostMapping("/signup")
+    public void signup() {
+
+    }
 }
