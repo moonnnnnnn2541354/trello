f642f3fadabf9e067c809733c9f195cf5b92ac12
minahYu <mina010311@naver.com>
2023-12-26 16:59:36 +0900

    feat : 사용자 엔티티 생성

---
+++ src/main/java/com/sparta/trellor/domain/user/entity/User.java
@@ -0,0 +1,36 @@
+package com.sparta.trellor.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String email, String password, UserRoleEnum role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
+
