645ce8ab752651c927bfb6a02bf064bf7d59a4fe
minahYu <mina010311@naver.com>
2023-12-26 20:37:51 +0900

    feat : jwt인증필터에 로그인 시도하는 메서드 추가

--- src/main/java/com/sparta/trellor/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/trellor/global/security/JwtAuthenticationFilter.java
@@ -1,8 +1,17 @@
 package com.sparta.trellor.global.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.trellor.domain.user.dto.request.LoginRequestDto;
 import com.sparta.trellor.global.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Slf4j(topic = "로그인 및 JWT 생성")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -11,5 +20,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
         setFilterProcessesUrl("/api/users/login");
+    }
+
+    /**
+     * 로그인을 시도하는 메서드
+     */
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws AuthenticationException {
+        log.info("로그인 시도");
+        try {
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword(),
+                            null
+                    )
+            );
+        } catch(IOException e) {
+            log.error("로그인 실패 : " + e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
