0163bcb720589d73c2384103d7ab5ebbb6af68bb
minahYu <mina010311@naver.com>
2023-12-26 20:14:44 +0900

    feat : JwtUtil 클래스에 헤더에서 jwt토큰 가져오는 메서드 추가

--- src/main/java/com/sparta/trellor/global/jwt/JwtUtil.java
+++ src/main/java/com/sparta/trellor/global/jwt/JwtUtil.java
@@ -5,8 +5,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Base64;
@@ -46,5 +49,17 @@ public class JwtUtil {
                         .setIssuedAt(date)
                         .signWith(key, signatureAlgorithm)
                         .compact();
+    }
+
+    /**
+     * request header에서 Jwt 토큰 가져오는 메서드
+     */
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
