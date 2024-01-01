8b9fd7a8bea556d54454f604ba6683c172813b95
An <taennos1017@naver.com>
2024-01-01 22:44:23 +0900

    feat: Exception 구현

---
+++ src/main/java/com/sparta/trellor/global/exception/WriterNotMatchException.java
@@ -0,0 +1,7 @@
+package com.sparta.trellor.global.exception;
+
+public class WriterNotMatchException extends RuntimeException{
+    public WriterNotMatchException(String msg){
+        super(msg);
+    }
+}
