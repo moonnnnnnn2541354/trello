9002aa4ec46b83eec873c25cabc2f1aaf4565511
minahYu <mina010311@naver.com>
2024-01-02 19:38:17 +0900

    feat : 사용자 응답메시지 관련 코드 추가

---
+++ src/main/java/com/sparta/trellor/domain/user/dto/response/UserMessageResponseDto.java
@@ -0,0 +1,28 @@
+package com.sparta.trellor.domain.user.dto.response;
+
+import com.sparta.trellor.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserMessageResponseDto {
+    private String msg;
+    private int status;
+    private UserResponseDto data;
+
+    @Builder
+    public UserMessageResponseDto(String msg, int status, User user) {
+        this.msg = msg;
+        this.status = status;
+        this.data = new UserResponseDto(user);
+    }
+
+    @Builder
+    public UserMessageResponseDto(String msg, int status) {
+        this.msg = msg;
+        this.status = status;
+    }
+
+}
