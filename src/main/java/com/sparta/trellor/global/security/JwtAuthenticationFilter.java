98d52f60e14f08f6837197a79dececbb01636f1b
tast <ioae7979@gmail.com>
2024-01-02 22:28:17 +0900

    style: global_jwt,security,config 주석문 제거 및 정렬

--- src/main/java/com/sparta/trellor/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/trellor/global/security/JwtAuthenticationFilter.java
@@ -2,25 +2,22 @@ package com.sparta.trellor.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.trellor.domain.user.dto.request.LoginRequestDto;
-import com.sparta.trellor.domain.user.dto.response.UserMessageResponseDto;
 import com.sparta.trellor.domain.user.entity.UserRoleEnum;
 import com.sparta.trellor.global.jwt.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.io.IOException;
-
 @Slf4j(topic = "로그인 및 JWT 생성")
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
     private final JwtUtil jwtUtil;
 
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
@@ -28,40 +25,35 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         setFilterProcessesUrl("/api/users/login");
     }
 
-    /**
-     * 로그인을 시도하는 메서드
-     */
     @Override
     public Authentication attemptAuthentication(
-            HttpServletRequest request,
-            HttpServletResponse response
+        HttpServletRequest request,
+        HttpServletResponse response
     ) throws AuthenticationException {
         log.info("로그인 시도");
         try {
-            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(),
+                LoginRequestDto.class);
 
             return getAuthenticationManager().authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            requestDto.getUsername(),
-                            requestDto.getPassword(),
-                            null
-                    )
+                new UsernamePasswordAuthenticationToken(
+                    requestDto.getUsername(),
+                    requestDto.getPassword(),
+                    null
+                )
             );
-        } catch(IOException e) {
+        } catch (IOException e) {
             log.error("로그인 실패 : " + e.getMessage());
             throw new RuntimeException(e.getMessage());
         }
     }
 
-    /**
-     * 인증 성공시 실행되는 메서드
-     */
     @Override
     protected void successfulAuthentication(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain chain,
-            Authentication authResult
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain chain,
+        Authentication authResult
     ) throws IOException {
         log.info("로그인 성공");
 
@@ -77,14 +69,11 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.getWriter().write("로그인 성공했습니다.");
     }
 
-    /**
-     * 인증 실패시 실행되는 메서드
-     */
     @Override
     protected void unsuccessfulAuthentication(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            AuthenticationException failed
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException failed
     ) throws IOException, ServletException {
         log.info("로그인 실패");
 
